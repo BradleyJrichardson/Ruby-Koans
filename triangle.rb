50794a60de454c4d036378442ea32aedd58b705e
BradleyJrichardson <brdl.richardson@gmail.com>
2019-04-25 10:45:01 +1000

    added solution 2

--- triangle.rb
+++ triangle.rb
@@ -13,13 +13,39 @@
 # and
 #   about_triangle_project_2.rb
 #
-def triangle(a, b, c)
-  array = [a, b, c].map{|side| side.odd?
-  :equilateral if array.length == 0
-  :isosceles if array.length == 1
-  :scalene if array.length == 2
-end
 
+
+# Solution 1
+def triangle(a, b, c)
+  raise TriangleError if [a,b,c].min <= 0
+  x, y, z = [a,b,c].sort
+  raise TriangleError if x + y <= z
+  array = [a, b, c]
+  array2 = array.select{|side| side.odd?}
+  puts :equilateral if array2.length == 0
+  puts :isosceles if array2.length == 1
+  puts :scalene if array2.length == 3
+end
 # Error class used in part 2.  No need to change this code.
 class TriangleError < StandardError
 end
+
+
+# Solution 2
+def triangle(a, b, c)
+  return :equilateral if a == b && a == c
+  return :isosceles if (a == b && a != c) || (b == c && b != a) || (a == c && a != b)
+  return :scalene
+end
+
+
+# Solution 3
+def triangle(a, b, c)
+  sides = [a,b,c].sort
+
+   raise TriangleError if sides.first <= 0 || sides[2] >= sides[1] + sides[0]
+   return :equilateral if sides.uniq.length  == 1
+   return :isosceles if sides.uniq.length  == 2
+   :scalene
+ end
+
